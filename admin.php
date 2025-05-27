35087f2555f6fdba498a7e23e18173b70bc12b4b
ZAMIR <zamirhuertas18@hotmail.com>
2025-05-27 17:07:04 -0500

    Fix: ajustar rutas y lógica en productos.php

--- admin.php
+++ admin.php
@@ -18,7 +18,7 @@ $productos = $pdo->query("SELECT * FROM producto")->fetchAll(PDO::FETCH_ASSOC);
 <head>
   <meta charset="UTF-8">
   <title>Panel Admin - Doggies</title>
-  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="css/Login.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
   <style>
     .admin-container {
