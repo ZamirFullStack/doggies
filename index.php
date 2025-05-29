d6b4ea0abde412b1d65bae1b49afbde1929f3f8f
ZAMIR <zamirhuertas18@hotmail.com>
2025-05-29 00:34:23 -0500

    Fix: uso correcto del SDK de MercadoPago

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="css/styles.css" />
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-  <link rel="icon" type="image/png" href="img/logo-doggies.png" />
+  <link rel="icon" type="image/jpeg" href="img/fondo.jpg" />
   <style>
     .submenu { position: relative; cursor: pointer; }
     .submenu-opciones {
