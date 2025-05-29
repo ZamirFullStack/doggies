65e3c577ac88c1a70a4b5aa9655973e16646ef50
ZAMIR <zamirhuertas18@hotmail.com>
2025-05-29 00:41:46 -0500

    Fix: uso correcto del SDK de MercadoPago

--- recover.php
+++ recover.php
@@ -61,6 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Recuperar contraseña</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link rel="icon" type="image/jpeg" href="img/fondo.jpg" />
   <style>
     * {
       margin: 0;
